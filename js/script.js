670cee12c594bfa4210498b953d4c68fe7131b7f
Jencyc26 <131987402+Jencyc26@users.noreply.github.com>
2026-05-10 00:51:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("EOH Studio Running");
+const imageUpload = document.getElementById("imageUpload");
+const preview = document.getElementById("preview");
+
+imageUpload.addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(file){
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+preview.innerHTML = `<img src="${e.target.result}">`;
+
+}
+
+reader.readAsDataURL(file);
+
+}
+
+});
