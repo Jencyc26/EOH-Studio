0b947165dfcc74e145a112406f5ef07db28df506
Jencyc26 <131987402+Jencyc26@users.noreply.github.com>
2026-05-10 23:06:29 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,12 @@
+function saveProjects(projects){
+localStorage.setItem(
+"eoh-projects",
+JSON.stringify(projects)
+);
+}
 
+function loadProjects(){
+return JSON.parse(
+localStorage.getItem("eoh-projects")
+) || [];
+}
